b230032d14a2c3b6aba8312963c26dfbc79ff5c4
danycalvo <danycalvo@gmail.com>
2021-09-16 14:11:01 -0300

    elimine el config de sqlalchemy

--- app/funciones.py
+++ app/funciones.py
@@ -11,8 +11,8 @@ def create_app(settings_module):
     app = Flask(__name__)
     app.config['DEBUG'] = True
     app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:testing@localhost:5432/miniblog'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:testing@localhost:5432/miniblog'
+#    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     # login_manager.init_app(app)
     #login_manager.login_view = "login"
